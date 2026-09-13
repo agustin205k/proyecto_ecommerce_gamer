f7d3cd76d22783d87e0887a1736c63fe9f0c6d8d
Agustin Solorzano <agustin205k@gmail.com>
2026-09-13 19:48:00 -0300

    about terminado

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,6 +1,5 @@
 /* Dependencies */
-import { Outlet } from "react-router-dom";
-import { Link } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 /* Local */
 import styles from "./layout.module.css";
@@ -10,17 +9,20 @@ function Layout(){
   <>
     <div className={styles.webPageLayout}>
       <header className={styles.webPageLayout__header}>
-        <ul>
-          <li><Link to={"/"}>inicio</Link></li>
-          <li><Link to={"/404"}>404</Link></li>
-          <li><Link to={"/about"}>About</Link></li>
-        </ul>
+        <nav className={styles.webPageLayout__nav}>
+          <ul className={styles.webPageLayout__navList}>
+            <li className={styles.webPageLayout__navLink}><Link to={"/"}>inicio</Link></li>
+            <li className={styles.webPageLayout__navLink}><Link to={"/about"}>About</Link></li>
+            <li className={styles.webPageLayout__navLink}><Link to={"/register"}>Register</Link></li>
+          </ul>
+        </nav>
+        
       </header>
       <div className={styles.webPageLayout__main}>
         <Outlet/>
       </div>
       <footer className={styles.webPageLayout__footer}>
-
+        <p>&copy;Developer</p>
       </footer>
     </div>
   </>
--- src/pages/About/about.tsx
+++ src/pages/About/about.tsx
@@ -1,7 +1,79 @@
+/* Local */
+import styles from "./about.module.css"
+import iconGame from "../../assets/icons/game.png";
+import iconCommunity from "../../assets/icons/community.png";
+import iconSecurity from "../../assets/icons/security.png";
+import iconUser from "../../assets/icons/user.png";
+import perfilAndrea from "../../assets/imgs/perfilAndrea.jpeg";
+import perfilAgustin from "../../assets/imgs/perfilAgustin.webp";
+import perfilGerardo from "../../assets/imgs/perfilGerardo.jpeg";
+import perfilAlan from "../../assets/imgs/perfilAlan.jpeg";
+
+
 function About(){
  return(
   <>
-    <h2>Soy el About</h2>
+    <div className={styles.about}>
+      <div className={styles.hero}>
+        <div className={styles.heroContainer}>
+          <div className={styles.hero__titleContainer}>
+            <h2 className={styles.hero__title}>Sobre Nosotros</h2>
+            <h3 className={styles.hero__subtitle}>Somos un equipo de gamer, para gamers</h3>
+          </div>
+          
+          <p className={styles.hero__p}>En <strong>Nexora Gaming</strong> creemos que los videojuegos son más que un hobby, son una forma de vida. Por eso, creamos esta plataforma para que puedan encontrar,comprar y disfrutar tus juegos favoritos de manera facil, seguro y rapido.</p>
+        </div>
+      </div>
+      <div className={styles.icons}>
+        <ul className={styles.iconsList}>
+          <li className={styles.iconsList__item}>
+            <img className={styles.iconsList__img} src={iconGame} alt="" />
+            <h4 className={styles.iconsList__title}>Pasion</h4>
+            <span className={styles.iconsList__span}>por los juegos</span>
+          </li>
+          <li className={styles.iconsList__item}>
+            <img className={styles.iconsList__img} src={iconSecurity} alt="" />
+            <h4 className={styles.iconsList__title}>Compras</h4>
+            <span className={styles.iconsList__span}>seguras</span>
+          </li>
+          <li className={styles.iconsList__item}>
+            <img className={styles.iconsList__img} src={iconUser} alt="" />
+            <h4 className={styles.iconsList__title}>Soporte</h4>
+            <span className={styles.iconsList__span}>personalizado</span>
+          </li>
+          <li className={styles.iconsList__item}>
+            <img className={styles.iconsList__img} src={iconCommunity} alt="" />
+            <h4 className={styles.iconsList__title}>Comunidad</h4>
+            <span className={styles.iconsList__span}>gamer</span>
+          </li>
+        </ul>
+      </div>
+      <div className={styles.team}>
+        <h2 className={styles.teamTitle}>Nuestro Equipo</h2>
+        <ul className={styles.teamList}>
+          <li className={styles.teamList__item}>
+            <img className={styles.teamList__img} src={perfilAndrea} alt="" />
+            <h4 className={styles.teamList__title}>Andrea Costas</h4>
+            <span className={styles.teamList__span}>Scrum Master</span>
+          </li>
+          <li className={styles.teamList__item}>
+            <img className={styles.teamList__img} src={perfilAgustin} alt="" />
+            <h4 className={styles.teamList__title}>Agustín Solórzano</h4>
+            <span className={styles.teamList__span}>Lider Tecnico</span>
+          </li>
+          <li className={styles.teamList__item}>
+            <img className={styles.teamList__img} src={perfilGerardo} alt="" />
+            <h4 className={styles.teamList__title}>Julio Gerardo Monteros</h4>
+            <span className={styles.teamList__span}>Programador</span>
+          </li>
+          <li className={styles.teamList__item}>
+            <img className={styles.teamList__img} src={perfilAlan} alt="" />
+            <h4 className={styles.teamList__title}>Alan Valdez</h4>
+            <span className={styles.teamList__span}>Programador</span>
+          </li>
+        </ul>
+      </div>
+    </div>
   </>
  );
 }
