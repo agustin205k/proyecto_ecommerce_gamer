0abf892bdf84c34db14a27c845e87a89b3b861c0
Alan Valdez <valdezalan9000@gmail.com>
2026-09-19 01:40:13 -0300

    contexto de usuarios

--- src/context/userContext/userContext.ts
+++ src/context/userContext/userContext.ts
@@ -1,3 +1,26 @@
 import { createContext } from "react";
 
-export const UserContext = createContext({});
+export type Rol = "admin" | "visitante";
+
+export interface Usuario {
+    id: string;
+    nombre:string;
+    email:string;
+    password:string;
+    rol: Rol;
+}
+
+interface UserContextType {
+  usuarios: Usuario[];
+  usuarioActual: Usuario | null;
+
+  registrarUsuario: (usuario: Usuario) => void;
+  eliminarUsuario: (id: string) => void;
+  editarUsuario: (usuario: Usuario) => void;
+  iniciarSesion: (email: string, password: string) => Usuario | null;
+  cerrarSesion: () => void;
+}
+
+export const UserContext = createContext<UserContextType>(
+  {} as UserContextType
+);
